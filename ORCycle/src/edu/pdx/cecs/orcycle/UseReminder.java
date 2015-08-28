a90b47193eb5413118c816df068855511e1ddb8b
Robin Murray <robin5@pdx.edu>
2015-08-28 16:52:40 -0700

    Changed 6 month use reminder to 3 months.
    Changed 6 month use reminder to 3 months.  Also refactored classes to
    make them more generic.

--- ORCycle/src/edu/pdx/cecs/orcycle/UseReminder.java
+++ ORCycle/src/edu/pdx/cecs/orcycle/UseReminder.java
@@ -8,63 +8,63 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class SixMonthReminder {
+public class UseReminder {
 
-	private static final String MODULE_TAG = "SixMonthReminder";
+	private static final String MODULE_TAG = "UseReminder";
 
-	public static final String EXTRA_REMINDER_NAME = "REMINDER_NAME";  // TODO: place in reciever
+	public static final int REMINDER_PERIOD = 3; // In months
 
 	/**
-	 * Creates an alarm intent for 6 months from now.
-	 * with action: ACTION_SIX_MONTH_REMINDER
+	 * Creates an alarm intent for reminder.
+	 * with action: ACTION_USE_REMINDER
 	 * @param context
 	 * @return
 	 */
-	private static PendingIntent getSixMonthAlarm(Context context) {
+	private static PendingIntent getUseAlarm(Context context) {
 
-		Intent intent = new Intent(ReminderReceiver.ACTION_SIX_MONTH_REMINDER);
+		Intent intent = new Intent(ReminderReceiver.ACTION_USE_REMINDER);
 
 		// Construct and return the pending intent for broadcasting the alarm
 		return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 	}
 
 	/**
-	 * Creates an alarm intent for 6 months from now.
+	 * Creates an alarm intent for 1 week from now.
 	 * with action: ACTION_ONE_WEEK_REMINDER
 	 * @param context
 	 * @return
 	 */
 	private static PendingIntent getOneWeekAlarm(Context context) {
 
-		Intent intent = new Intent(ReminderReceiver.ACTION_ONE_WEEK_REMINDER);
+		Intent intent = new Intent(ReminderReceiver.ACTION_ONE_WEEK_USE_REMINDER);
 
 		// Construct and return the pending intent for broadcasting the alarm
 		return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 	}
 
 	/**
-	 * Cancels the current 6 month reminder, and schedules a new one for 6 months from now
+	 * Cancels the current use reminder, and schedules a new one.
 	 * @param context
 	 */
-	public static void rescheduleSixMonthAlarm(Context context) {
+	public static void rescheduleAlarm(Context context) {
 
 		try {
 			AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
 			// Cancel all previous alarms for this reminder
-			alarmMgr.cancel(getSixMonthAlarm(context));
+			alarmMgr.cancel(getUseAlarm(context));
 
 			// Create a calendar set to the current time
 			Calendar alarmCalendar = Calendar.getInstance();
 			alarmCalendar.setTimeInMillis(System.currentTimeMillis());
 
-			// Add six months to the calendar
-			alarmCalendar.add(Calendar.MONTH, 6);
-			//alarmCalendar.add(Calendar.MINUTE, 1);
+			// Add REMINDER_PERIOD months to the calendar
+			alarmCalendar.add(Calendar.MONTH, REMINDER_PERIOD);
+			// alarmCalendar.add(Calendar.MINUTE, 1); // For debugging
 
 			Log.i(MODULE_TAG, "Alarm set for: " + alarmCalendar.getTime().toString());
 
-			PendingIntent alarmIntent = getSixMonthAlarm(context);
+			PendingIntent alarmIntent = getUseAlarm(context);
 
 			// Set alarm to go off at the time set by the calendar
 			alarmMgr.set(AlarmManager.RTC, alarmCalendar.getTimeInMillis(), alarmIntent);
@@ -92,7 +92,7 @@ public class SixMonthReminder {
 
 			// Add six months to the calendar
 			alarmCalendar.add(Calendar.HOUR, 24 * 7);
-			//alarmCalendar.add(Calendar.SECOND, 30);
+			// alarmCalendar.add(Calendar.SECOND, 30); // For debugging
 
 			Log.i(MODULE_TAG, "Alarm set for: " + alarmCalendar.getTime().toString());
 
